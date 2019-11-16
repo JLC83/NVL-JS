1af219921ff346608835fd73a881b6138313e617
JLC83 <55483682+JLC83@users.noreply.github.com>
2019-11-16 20:38:26 +0100

    Add files via upload

---
+++ P9b.js
@@ -0,0 +1,7 @@
+var dias="Lunes,Martes,Miércoles,Jueves,Viernes,Sábado,Domingo";
+var ciudades="Madrid.Sevilla.Barcelona.Valencia.Zaragoza.Bilbao.Donosti";
+
+var conciertos;
+for(var i=0;i++;i<dias.length()){
+ 	conciertos=[[dias[i].split(","),ciudades[i].split(".")]]
+ }
