23c3a60db797a659031d2d10d9f084dc9cb98134
JLC83 <55483682+JLC83@users.noreply.github.com>
2019-11-16 20:50:54 +0100

    Add files via upload

---
+++ P10b.js
@@ -0,0 +1,14 @@
+var numeros, pares, impares;
+var j=0;var z=0;
+for(var i=0;i<50;i++){
+	numeros[i]=Math.floor((Math.random() * 100) + 1);
+	if numeros[i]%2==0{
+		pares[j]=numeros[i];
+		j++;
+		}
+		else{
+			impares[z]=numeros[i];
+			z++;
+		}
+}
+
